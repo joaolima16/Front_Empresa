69806b30030fc61d0c4e5034710f7c88bef445f6
João Vitor De Lima <88281625+joaolima16@users.noreply.github.com>
2023-06-21 13:40:02 -0300

    falta terminar uma tela só, Oh Glória!!!!!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import './App.css';
-import Home from './Pages/Home/Index';
 import RoutesPage from './Routes/Routes';
 
 function App() {
--- src/Pages/ListObras/index.js
+++ src/Pages/ListObras/index.js
@@ -1,44 +1,86 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
+import teste from "../../assets/images/Obra.PNG";
+import teste2 from "../../assets/images/obra2.PNG";
+import teste3 from "../../assets/images/obra3.PNG";
 import "./style.css";
-import { ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
-import teste from "../../assets/images/Imagem4.jpg";
-import teste2 from "../../assets/images/Imagem4.jpg";
-import teste3 from "../../assets/images/Imagem4.jpg";
+import "react-multi-carousel/lib/styles.css";
+import { Link } from "react-router-dom";
+import { IoArrowBackCircleOutline } from "react-icons/io5";
 
 export default function ListObrasPage() {
-
-    const itemData = [
-        {img: teste, title: "teste1"},
-        {img: teste3, title: "teste2"},
-        {img: teste2, title: "teste13"},
-        {img: teste2, title: "teste13"},
-        {img: teste2, title: "teste13"},
-        {img: teste2, title: "teste13"},
-        {img: teste2, title: "teste13"},
-        {img: teste2, title: "teste13"},
-        
-    ]
+  const itemData = [
+    { img: teste, title: "teste1" },
+    { img: teste2, title: "teste2" },
+    { img: teste3, title: "teste13" },
+  ];
   return (
     <>
-      <header className="Header"></header>
+      <header className="Header_obra">
+        <Link to="/" className="Link_Page_Obra">
+          <IoArrowBackCircleOutline className="Link_Home" />
+        </Link>
+        <h3 className="Title_Header">Portfólio de Obras</h3>
+      </header>
       <section className="Container_List">
-      <ImageList sx={{ width: 800, height: 700 }}>
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={item.title}
-            subtitle={<span>by: {item.author}</span>}
-            position="below"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+        <Carousel
+          additionalTransfrom={0}
+          arrows
+          autoPlay
+          autoPlaySpeed={2000}
+          centerMode={false}
+          className=""
+          containerClass="container-with-dots"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          pauseOnHover
+          renderArrowsWhenDisabled={false}
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024,
+              },
+              items: 3,
+              partialVisibilityGutter: 40,
+            },
+            mobile: {
+              breakpoint: {
+                max: 464,
+                min: 0,
+              },
+              items: 1,
+              partialVisibilityGutter: 30,
+            },
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 464,
+              },
+              items: 2,
+              partialVisibilityGutter: 30,
+            },
+          }}
+          rewind={false}
+          rewindWithAnimation={false}
+          rtl={false}
+          shouldResetAutoplay
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={2}
+          swipeable
+        >
+          {itemData.map((index) => {
+            return <img src={index.img} className="Image" />;
+          })}
+        </Carousel>
       </section>
     </>
   );
